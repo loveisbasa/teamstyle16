110e2fddfc3b1b096bb28510f1ad38ef10d11d89
王健宇 <rickidynamic@gmail.com>
2014-07-26 20:22:59 +0800

    delete useless comment

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -16,38 +16,4 @@
         |
         <a href="<?php echo URL; ?>login/requestpasswordreset">Forgot my Password</a>
     </div>
-    <!-- main content output -->
-   <!--  <div>
-        <h3>Amount of songs (data from second model)</h3>
-        <div>
-            <?php echo $amount_of_songs; ?>
-        </div>
-        <h3>List of songs (data from first model)</h3>
-        <table>
-            <thead style="background-color: #ddd; font-weight: bold;">
-            <tr>
-                <td>Id</td>
-                <td>Artist</td>
-                <td>Track</td>
-                <td>Link</td>
-                <td>DELETE</td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($songs as $song) { ?>
-                <tr>
-                    <td><?php if (isset($song->id)) echo $song->id; ?></td>
-                    <td><?php if (isset($song->artist)) echo $song->artist; ?></td>
-                    <td><?php if (isset($song->track)) echo $song->track; ?></td>
-                    <td>
-                        <?php if (isset($song->link)) { ?>
-                            <a href="<?php echo $song->link; ?>"><?php echo $song->link; ?></a>
-                        <?php } ?>
-                    </td>
-                    <td><a href="<?php echo URL . 'songs/deletesong/' . $song->id; ?>">x</a></td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-    </div>
-</div> -->
+   

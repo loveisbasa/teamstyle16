d6f78c9a63f310555861c42b637b4b64e2ae4a85
Nie Hao <niehao100@gmail.com>
2014-08-16 00:38:47 +0800

    完善了sql数据库的建立文件，基本完成message的发信部分

--- application/models/message_model.php
+++ application/models/message_model.php
@@ -13,7 +13,16 @@ class MessageModel()
 
 	public function SendMessage()
 	{
+		if(!isset($_POST['user_to_nickname']) OR empty($_POST['user_to_nickname'])){
+			$_SESSION["feedback_negative"][]=FEEDBACK_USERNAME_TO_FIELD_EMPTY;
+				return false;		
+		}
+		elseif(!isset($_POST['message']) OR empty($_POST['message'])){
+			$_SESSION["feedback_negative"][]=FEEDBACK_MESSAGE_FIELD_EMPTY;
+		}
+		else{
 
+		
 	}
 	public function ReadMessage()
 	{

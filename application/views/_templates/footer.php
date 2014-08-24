7c6dbe8fea63ca8c1b991bcd5c70f7542072694a
loveisbasa <haoyuetang@126.com>
2014-08-24 22:49:19 +0800

    header and footer

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -2,39 +2,26 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
 <hr>
+<style type="text/css">
+h6 {text-align:center;}
+</style>
 
-<div class="navbar  navbar-default" role="navigation">
-      <div class="container">
-        <div class="navbar-footer">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="<?php echo URL; ?>">联系我们</a></li>
-          </ul>
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="<?php echo URL; ?>">平台报错</a></li>
-	      </ul>
-	      <ul class="nav navbar-nav">
-            <li calss="active"><a href="<?php echo URL; ?>">关于队式</a></li>
-          </ul>
+<footer class="bs-docs-footer" role="contentinfo">
+  <div class="row">
+    <div class="col-md-12">
+    
 
-<a class="navbar-brand navbar-right" href="<?php echo URL; ?>">TeamStyle 16</a>
-          
-        </div><!--/.nav-collapse -->
-      </div>
-    </div> 
-
-     <script src="<?php echo URL;?>public/js/footer.js"></script>
-<!--     <script src="<?php echo URL;?>public/js/jquery-1.11.1.min.js"></script>
-     <script src="<?php echo URL;?>public/js/bootstrap.min.js"></script>
-     <script src="<?php echo URL;?>public/js/jquery.leanModal.min.js"></script>-->
-
-  </body>
+    <ul class="footer-list">
+      <h6>当前版本： v1.0.0</h6>
+      <ol style="list-style-type:none;margin:auto;width:30%">
+        <li style="float:left;width:8em";><a href="<?php echo URL;?>">关于队式</a></li>
+        <li style="float:left;width:8em";><a href="<?php echo URL;?>">平台报错</a></li>
+        <li style="float:left;width:7em";><a href="<?php echo URL;?>">联系我们</a></li>
+      </ol>
+      <br>
+    <h6>队式十六网站组荣誉出品</h6> 
+    </ul>
+  </div>
+  </div>
+</footer>
 </html>

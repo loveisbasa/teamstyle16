fab193456ffbc9486d24cfa16dec2ca5d8d30778
loveisbasa <haoyuetang@126.com>
2014-09-02 16:33:05 +0800

    decorate footer

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,11 +1,9 @@
 
-
-<hr>
 <style type="text/css">
 h6 {text-align:center;}
 </style>
-
-<footer class="bs-docs-footer" role="contentinfo">
+<link href="<?php echo URL; ?>public/css/docs.min.css" rel="stylesheet">
+<!--<footer class="bs-docs-footer" role="contentinfo">
   <div class="row">
     <div class="col-sm-12">
     
@@ -24,7 +22,7 @@ h6 {text-align:center;}
     </ul>
   </div>
   </div>
-</footer>
+</footer>-->
 
  <script src="<?php echo URL;?>public/js/jquery-1.11.1.min.js"></script>
 <script src="<?php echo URL;?>public/js/bootstrap.min.js"></script>
@@ -39,4 +37,21 @@ $(function(){
   $("a[rel*=leanModal]").leanModal();
 });
 </script>
+<footer class="bs-docs-footer" role="contentinfo">
+  <div class="container">
+    
+
+    <p>Designed and built with all the love in the world by 队式十六网站组</p>
+    <ul class="bs-docs-footer-links muted">
+      <li>当前版本： v1.2.0</li>
+      <li>&middot;</li>
+      <li><a href="<?php echo URL;?>">关于队式</a></li>
+      <li>&middot;</li>
+      <li><a href="<?php echo URL;?>">平台报错</a></li>
+      <li>&middot;</li>
+      <li><a href="<?php echo URL;?>">联系我们</a></li>
+      <li>&middot;</li>
+    </ul>
+  </div>
+</footer>
 </html>

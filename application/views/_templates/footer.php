9810bcd7d1e2318ea86271528c5ae8ccb71b6262
Nie Hao <niehao100@gmail.com>
2014-09-02 17:00:00 +0800

    footer增加时间显示

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -42,6 +42,38 @@ $(function(){
     
 
     <p>Designed and built with all the love in the world by 队式十六网站组</p>
+		<p id="time">当前时间</p>
+		<script >
+function showtime()
+{
+	var today,hour,second,minute,year,month,date;
+	var strDate ;
+	today=new Date();
+	var n_day = today.getDay();
+	switch (n_day)
+	{
+		    case 0:{strDate = "星期日"	}break;
+				case 1:{strDate = "星期一"	}break;
+				case 2:{strDate ="星期二"		}break;
+				case 3:{strDate = "星期三"	}break;
+				case 4:{strDate = "星期四"}break;
+				case 5:{strDate = "星期五"}break;
+				case 6:{strDate = "星期六"}break;
+				case 7:{strDate = "星期日"}break;
+	}
+	year = today.getFullYear();
+	month = today.getMonth()+1;
+	date = today.getDate();
+	hour = today.getHours();
+	minute =today.getMinutes();
+	second = today.getSeconds();
+	document.getElementById('time').innerHTML = year + "年" + month + "月" + date + "日" + strDate +" " + hour + ":" + minute + ":" + second; //显示时间
+	setTimeout("showtime();", 1000); //设定函数自动执行时间为 1000 ms(1 s)
+}
+		 showtime();
+</script>
+
+
     <ul class="bs-docs-footer-links muted">
       <li>当前版本： v1.2.0</li>
       <li>&middot;</li>

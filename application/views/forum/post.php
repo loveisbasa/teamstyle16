7646fd3c6b784d7cc862346e83f7c049cd0fbb0f
Nie Hao <niehao100@gmail.com>
2014-09-04 23:48:37 +0800

    forum调试完毕，写了一个很简单的界面

--- application/views/forum/post.php
+++ application/views/forum/post.php
@@ -14,5 +14,18 @@
         </div>
     </div> 
 
-</div> 
 <?php } ?>
+
+   <h3>Create Forums</h3>
+        <form action="<?php echo URL; ?>forum/create_post_action" method="post" class="auth-form form-horizontal">
+           <div class="form-field">
+            <input type="text" name="message" placeholder='回复' autocomplete="off" value="" required class="form-control name" />
+            <span class="icon icon-user-bold"></span>
+          </div>
+						<input type="radio" checked='checked' name='thread_id' value="<?php echo $thread_id; ?>"/>确认
+
+          <button type="submit" onclick="_hmt.push(['_trackEvent', 'signup_submit', 'click'])" class="btn btn-primary btn-large">
+           创建 
+          </button>
+        </form>
+</div>
--- application/views/forum/thread.php
+++ application/views/forum/thread.php
@@ -8,12 +8,30 @@
             <div class="panel-body">
                 <ul>
 									   
-										<li><?php echo "创建于" . $threads->first . "最新回复于" . $threads->last .  "共有" . $threads->response . "个帖子"; ?></li> 
+										<li><?php echo "创建于  " . $threads->first . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;最新回复于" . $threads->last .  "<br/>共有" . $threads->response . "个回复"; ?></li> 
 										<li><a href="<?php echo URL . 'forum/posts/' . $threads->thread_id; ?>">点击查看</a></li>
                 </ul>
             </div>
         </div>
     </div> 
 
-</div> 
 <?php } ?>
+</div class="col-sm-6">
+<div class= 
+   <h3>Create threads</h3>
+        <form action="<?php echo URL; ?>forum/create_thread_action" method="post" class="auth-form form-horizontal">
+           <div class="form-field">
+            <input type="text" name="thread_subject" placeholder='话题' autocomplete="off" value="" required class="form-control name" />
+            <span class="icon icon-user-bold"></span>
+          </div>
+          <div class="form-field">
+					<input type="text" placeholder="消息" value="" name="message" required class="form-control name" />
+						<span class="icon icon-envelope-bold"></span>
+          </div>
+						<input type="radio" checked='checked' name="forum_id" value="<?php echo $forum_id; ?>"/>确认
+<!--不太明白如何不用input来post一个值-->
+					<button type="submit" onclick="_hmt.push(['_trackEvent', 'signup_submit', 'click'])" class="btn btn-primary btn-large">
+           创建 
+          </button>
+        </form>
+</div>

91e0403d2a60ec698ce869a30d96a38790f29928
loveisbasa <haoyuetang@126.com>
2014-09-18 12:57:34 +0800

    decorate forum-index, add page rolling function to the team_display

--- application/views/team/createam.php
+++ application/views/team/createam.php
@@ -26,39 +26,39 @@
         <div class="brand">
           <h1>Teamstyle16 深蓝</h1>
         </div>
-        <h3>登录你的队式战队</h3>
+        <h3>创建你的队式战队</h3>
         <form action="<?php echo URL; ?>team/create_action" method="post" class="auth-form form-horizontal">
           <div class="face"></div>
           
           <input type="hidden" name="source" value="">
           <div class="form-field">
-            <input type="text" name="team_name" placeholder="Your teamname" autocomplete="off" value="" required class="form-control email" autofocus />
+            <input type="text" name="team_name" placeholder="队伍名" autocomplete="off" value="" required class="form-control email" autofocus />
             <span class="icon icon-envelope-bold"></span>
           </div>
           <div class="form-field">
-            <input type="password" name="team_password_new" placeholder='Team Password' autocomplete="off" value="" required class="form-control name" />
+            <input type="password" name="team_password_new" placeholder='加队密码' autocomplete="off" value="" required class="form-control name" />
             <span class="icon icon-user-bold"></span>
           </div>
           <div class="form-field">
-            <input type="password" name="team_password_repeat" placeholder='Team Password Again' autocomplete="off" value="" required class="form-control name" />
+            <input type="password" name="team_password_repeat" placeholder='确认密码' autocomplete="off" value="" required class="form-control name" />
             <span class="icon icon-user-bold"></span>
           </div>
           <div class="form-field">
-            <input type="text" name="team_slogan" placeholder="Your team slogan" autocomplete="off" value="" required class="form-control email" autofocus />
+            <input type="text" name="team_slogan" placeholder="队伍口号" autocomplete="off" value="" required class="form-control email" autofocus />
             <span class="icon icon-envelope-bold"></span>
           </div>
           		<!--TODO:运用JavaScript做出动态菜单的效果-->
           <div class="form-field">
-         <input type = "text" name = "team_member1" placeholder="Your teammate 1"  class="form-control name">
+         <input type = "text" name = "team_member1" placeholder="队友1昵称（可不填）"  class="form-control name">
           </div>
           <div class="form-field">
-		<input type = "text" name = "team_member2" placeholder="Your teammate 2"  class="form-control name">
+		<input type = "text" name = "team_member2" placeholder="队友2昵称（可不填）"  class="form-control name">
 	</div>
           <button type="submit" onclick="_hmt.push(['_trackEvent', 'signup_submit', 'click'])" class="btn btn-primary btn-large">
             立刻建队
           </button>
           <div class="action-wrapper">
-            <a href="/login" class="signup pull-right">Already have an account?</a>
+            <a href="<?php echo URL. 'team/team_display'?>" class="signup pull-right">加入其他战队？</a>
           </div>
         </form>
       </div>

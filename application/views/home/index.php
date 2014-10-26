1b75fd08fd47b6f7022fd6f4ffd7635742a232fe
loveisbasa <haoyuetang@126.com>
2014-10-26 23:44:07 +0800

    rectify frontpage bugs

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,10 +1,12 @@
 <link href="<?php echo URL; ?>public/css/bootstrap.css" rel="stylesheet">
-<div class="jumbotron" style = "position:relative;top:-20px;background:url(<?php echo URL; ?>public/img/jiemian.jpg)">
-    <br/>
-    <h1 style="text-indent:10px">Deep Blue 深蓝<small>TeamStyle16 </small></h1>
+<div class="jumbotron" style = "position:relative;top:-20px;background:url(<?php echo URL; ?>public/img/jiemian.jpg);background-repeat:no-repeat;background-position:center">
+    <div class="container">
+        <br>
+    <h1 style="position:relative;top:-30px;text-indent:10px">深蓝<small>TeamStyle16 </small></h1>
     <br><br><br><br><br><br><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     <p>&nbsp;&nbsp;&nbsp;&nbsp;<strong>苍烟缭绕，云泽浩莽，有大洋名洪荒。</strong></p>
     <?php echo "&nbsp;&nbsp;&nbsp;&nbsp;";?><a href="<?php echo URL. 'login/register'?>" class="btn btn-primary btn-lg" role="button">现在就加入!  &raquo;</a>
+</div>
 </div>
         <div class="tm-section">
             <div class="uk-container uk-container-center uk-text-center">

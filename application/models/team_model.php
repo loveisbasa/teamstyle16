3e0100918eeaf0ab93099ff3dad68d14acce8ba2
loveisbasa <haoyuetang@126.com>
2015-02-25 03:17:03 -0800

    翻页功能障碍

--- application/models/team_model.php
+++ application/models/team_model.php
@@ -225,8 +225,7 @@ class TeamModel
 		$query = $this->db->prepare("SELECT u1.user_nickname as team_captain,team_id, team_name, team_slogan, u2.user_nickname as team_member1, u3.user_nickname as team_member2, team_full
 			FROM teams inner JOIN users as u1 on teams.team_captain=u1.user_id 
 			left JOIN users as u2 on teams.team_member1=u2.user_id
-		  left JOIN users as u3 on teams.team_member2=u3.user_id
-		  WHERE (team_id>8*$page-8) AND (team_id<8*$page)");
+		  left JOIN users as u3 on teams.team_member2=u3.user_id");
 		$query->execute();
 		$result = $query->fetchAll();
 		return $result;
